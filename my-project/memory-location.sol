b9c150562204e671c7fc2753f0fd5ba0c14db720
coolistkovbel <coolistkovbel@gmail.com>
2022-06-08 18:17:57 -0400

    cool challange

---
+++ my-project/memory-location.sol
@@ -0,0 +1,13 @@
+pragma solidity 6.7.5;
+
+contract memory-location {
+
+
+  struct cars {
+    string typeOfCar;
+    uint specialId;
+  }
+
+
+
+}
---
+++ my-project/script.js
@@ -0,0 +1,42 @@
+let a = 7;
+let b = a;
+b = 10
+
+
+function changeValue(num) {
+  console.log(num)
+  num = 10
+  console.log(num)
+}
+
+// let c = 10;
+// changeValue(c)
+// console.log(c)
+
+
+let arrOne = [1,2,3,4,5]
+console.log(arrOne[3])
+arrOne[1] = 10
+arrOne[2] = 20
+arrOne[5] = 20
+console.log(arrOne)
+
+// for(let placeholder of arrOne){
+//   console.log(placeholder)
+// }
+
+
+// Make an array
+let arrayTwo = [1,2,3,4,5,6,7,8,9]
+// loop through array
+for(let i =0; i <= arrayTwo.length - 1; i++){
+
+  if(arrayTwo[i] % 2){
+    // Log number if its equal to the number
+    console.log(`The number ${i} is even.`)
+  } else{
+    console.log(`The number ${i} is odd`)
+  }
+
+}
+
