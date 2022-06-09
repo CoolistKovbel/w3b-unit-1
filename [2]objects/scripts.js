8bce51b41f5882c589dda85933dbf1bb43672b96
coolistkovbel <coolistkovbel@gmail.com>
2022-06-09 13:43:06 -0400

    fin i gues

--- [2]objects/scripts.js
+++ [2]objects/scripts.js
@@ -27,27 +27,36 @@ const places = [
 ]
 
 // looping
-const naCitties = []
+// const naCitties = []
 
 // Challange
 // Add a neighborhood key to each object as you loop
 
 
-for(let i = 0; i <places.length; i++){
-  // Random Key
-  const deKey = Math.floor(Math.random() * places.length)
+// for(let i = 0; i <places.length; i++){
+//   // Random Key
+//   const deKey = Math.floor(Math.random() * places.length)
 
-  places[i].neghborhoodKey = deKey
+//   places[i].neighborhoodKey = deKey
 
-  if(places[i].region === 'North America') naCitties.push(places[i])
+//   if(places[i].region === 'North America') naCitties.push(places[i])
+// }
+
+// console.log(places)
+// console.log(naCitties)
+
+
+
+
+// Most Realistic Way
+function placeMaker(array, index, neighborhood) {
+
+  array[index].neighborhood = neighborhood
+
+  return array
+
 }
 
-console.log(places)
-console.log(naCitties)
-
-
-
-
-
+console.log(placeMaker(places, 0, '123'))
 
 
