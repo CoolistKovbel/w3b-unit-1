ad94554ad077f971b303ac82aad971007f9cf04a
coolistkovbel <coolistkovbel@gmail.com>
2022-06-21 13:58:54 -0400

    day:09 i guess maybe

--- [6]Classes/MiniGame.js
+++ [6]Classes/MiniGame.js
@@ -13,3 +13,36 @@ class ZombieGame {
 const newGame = new ZombieGame({userName: 'lyub12'})
 
 console.log(newGame.gameStart())
+
+
+class ZombieClass {
+  constructor(props){
+    this.health = 100
+    this.name = props.name
+    this.power = props.power
+  }
+
+  stats(){
+    return `Zombie ${this.name} has a power level of ${this.power} and health of ${this.health}`
+  }
+
+  attack(recipient){
+    recipient.health = recipient.health - this.power / 100;
+    return `${this.power/100} damage has been given to ${recipient.name} from ${this.name}`
+  }
+
+}
+
+
+const stanley = new ZombieClass({name: 'stanley', power: 100})
+const bot = new ZombieClass({name: "jeff", power: 150})
+
+console.log(bot.stats())
+
+console.log(stanley.attack(bot))
+console.log(stanley.attack(bot))
+console.log(stanley.attack(bot))
+
+console.log(bot.health)
+
+console.log(bot.stats())
